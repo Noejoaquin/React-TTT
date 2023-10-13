800a220297514939c0cde374cab71ab96af67d6c
Noe <noe.joaquin.james@gmail.com>
2023-10-13 13:17:16 -0400

    feat: make ai class, make ai block winning move

--- src/App.js
+++ src/App.js
@@ -74,6 +74,58 @@ function Board({xIsNext, tiles, onPlay}) {
   );
 }
 
+class AIPlayer {
+  constructor(mark = 'O') {
+    k: this.mark = mark;
+    // blockingMove: arrayOfPossibleWinningScenariosForOpponent
+    this.blockingMap = {
+      0: [[1, 2], [3, 6], [4, 8]],
+      1: [[0, 2], [4, 7]],
+      2: [[0, 1], [5, 8], [4, 6]],
+      3: [[4, 5], [0, 6]],
+      4: [[3, 5], [1, 7], [2, 6], [0, 8]],
+      5: [[3, 4], [2, 8]],
+      6: [[7, 8], [0, 3], [2, 4]],
+      7: [[6, 8], [1, 4]],
+      8: [[6, 7], [2, 5], [0, 4]],
+    }
+  }
+
+  move(tiles) {
+    let newTiles = tiles.slice();
+    // make a move by iterating through the tiles, and placing a marker at the first null space
+    const blockingMove = this.#needBlockingMove(newTiles);
+    console.log(blockingMove, 'blocking Move');
+    if (blockingMove) {
+      newTiles[blockingMove] = this.mark;
+    } else {
+      // for now we will move where it is first available if there is no blocking move
+      // TODO: create a winning move function. -- will probably use the blockingMap again for that one.
+      for(let i=0; i < 9; i += 1) {
+        if (newTiles[i] === null) {
+          newTiles[i] = 'O'
+          break;
+        } 
+      }
+    }
+    return newTiles;
+  }
+
+  #needBlockingMove(tiles) {
+    // iterate through all the blocking moves
+    for (const blockingMove in this.blockingMap) {
+      const combosOfTilesToBlock = this.blockingMap[blockingMove];
+      // iterate through the set of possible opponent scenarios
+      for (let i=0; i < combosOfTilesToBlock.length; i += 1) {
+        const tilesToBlock = combosOfTilesToBlock[i];
+        // return the blocking move for the first scenario seen on the board
+        if (tiles[tilesToBlock[0]] === 'X' && tiles[tilesToBlock[1]] === 'X' && !tiles[blockingMove]) return blockingMove;
+      }
+    }
+    return null;
+  }
+}
+
 function Game() {
   const [opponentSelected, setOpponentSelected] = useState(false);
   const [isAi, setIsAi] = useState(false);
@@ -82,6 +134,7 @@ function Game() {
   const [history, setHistory] = useState([new Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);  
   let currentTiles = history[currentMove];
+  let aiPlayer = null;
 
   function handleOpponentSelected() {
     setOpponentSelected(!opponentSelected);
@@ -116,19 +169,11 @@ function Game() {
   })
   
   // TODO: Make AI smarter
-  if (isAi && !xIsNext && !calculateWinner(currentTiles)) {
-    const newTiles = currentTiles.slice();
-    // make a move by iterating through the tiles, and placing a marker at the first null space
-    for (let i=0; i < 9; i+=1) {
-      if (newTiles[i] === null) {
-        newTiles[i] = 'O'
-        break;
-      }
-    }
-    const nextHistory = [...history.slice(0, currentMove + 1), newTiles]
-    setHistory(nextHistory) // sets the new history
-    setCurrentMove(nextHistory.length - 1)
-    setXIsNext(!xIsNext);
+  if (isAi && !aiPlayer) aiPlayer = new AIPlayer();
+  if (aiPlayer && !xIsNext && !calculateWinner(currentTiles)) {
+    const newTiles = aiPlayer.move(currentTiles)
+    console.log(newTiles, 'newTILES');
+    handlePlay(newTiles);
   }
   
   return(
